a0628eab80e1be8ac59a15f2b890a8f1a09b1cca
hsu <frostbolt817@gmail.com>
2023-03-20 16:17:19 +0800

    chore: 添加注释

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -1,3 +1,4 @@
+// 引入相關模組
 import * as dotenv from 'dotenv'
 import 'isomorphic-fetch'
 import type { ChatGPTAPIOptions, ChatMessage, SendMessageOptions } from 'chatgpt'
@@ -6,10 +7,13 @@ import { SocksProxyAgent } from 'socks-proxy-agent'
 import { HttpsProxyAgent } from 'https-proxy-agent'
 import fetch from 'node-fetch'
 import axios from 'axios'
+
+// 引入自定義工具函數和相關類型
 import { sendResponse } from '../utils'
 import { isNotEmptyString } from '../utils/is'
 import type { ApiModel, ChatContext, ChatGPTUnofficialProxyAPIOptions, ModelConfig } from '../types'
 
+// 定義錯誤代碼和對應的提示信息
 const ErrorCodeMessage: Record<string, string> = {
   401: '[OpenAI] 提供错误的API密钥 | Incorrect API key provided',
   403: '[OpenAI] 服务器拒绝访问，请稍后再试 | Server refused to access, please try again later',
@@ -19,12 +23,15 @@ const ErrorCodeMessage: Record<string, string> = {
   500: '[OpenAI] 服务器繁忙，请稍后再试 | Internal Server Error',
 }
 
+// 設置環境變量
 dotenv.config()
 
+// 設定 timeout 時間
 const timeoutMs: number = !isNaN(+process.env.TIMEOUT_MS) ? +process.env.TIMEOUT_MS : 30 * 1000
 
 let apiModel: ApiModel
 
+// 如果缺少必要的 OPENAI_API_KEY 或 OPENAI_ACCESS_TOKEN 環境變量，則拋出錯誤
 if (!process.env.OPENAI_API_KEY && !process.env.OPENAI_ACCESS_TOKEN)
   throw new Error('Missing OPENAI_API_KEY or OPENAI_ACCESS_TOKEN environment variable')
 
@@ -35,6 +42,7 @@ let api: ChatGPTAPI | ChatGPTUnofficialProxyAPI
 
   if (process.env.OPENAI_API_KEY) {
     const OPENAI_API_MODEL = process.env.OPENAI_API_MODEL
+    // 如果OPENAI_API_MODEL存在，則使用它，否則使用'gpt-3.5-turbo'作為默認值
     const model = isNotEmptyString(OPENAI_API_MODEL) ? OPENAI_API_MODEL : 'gpt-3.5-turbo'
 
     const options: ChatGPTAPIOptions = {
@@ -43,6 +51,7 @@ let api: ChatGPTAPI | ChatGPTUnofficialProxyAPI
       debug: true,
     }
 
+    // 如果OPENAI_API_BASE_URL存在，則使用它設置apiBaseUrl
     if (isNotEmptyString(process.env.OPENAI_API_BASE_URL))
       options.apiBaseUrl = process.env.OPENAI_API_BASE_URL
 
@@ -52,11 +61,12 @@ let api: ChatGPTAPI | ChatGPTUnofficialProxyAPI
     apiModel = 'ChatGPTAPI'
   }
   else {
+    // 否則使用非官方代理 API
     const options: ChatGPTUnofficialProxyAPIOptions = {
       accessToken: process.env.OPENAI_ACCESS_TOKEN,
       debug: true,
     }
-
+    // 如果API_REVERSE_PROXY存在，則使用它設置apiReverseProxyUrl
     if (isNotEmptyString(process.env.API_REVERSE_PROXY))
       options.apiReverseProxyUrl = process.env.API_REVERSE_PROXY
 
@@ -67,6 +77,7 @@ let api: ChatGPTAPI | ChatGPTUnofficialProxyAPI
   }
 })()
 
+// 處理聊天回復
 async function chatReplyProcess(
   message: string,
   lastContext?: { conversationId?: string; parentMessageId?: string },
@@ -88,40 +99,48 @@ async function chatReplyProcess(
         process?.(partialResponse)
       },
     })
-
+    // 将响应发送给前端进行处理
     return sendResponse({ type: 'Success', data: response })
   }
   catch (error: any) {
     const code = error.statusCode
     global.console.log(error)
+    // 如果发生了常见错误码，则返回对应的错误信息
     if (Reflect.has(ErrorCodeMessage, code))
       return sendResponse({ type: 'Fail', message: ErrorCodeMessage[code] })
+    // 否则，返回通用的错误信息
     return sendResponse({ type: 'Fail', message: error.message ?? 'Please check the back-end console' })
   }
 }
 
+// 获取当前账户余额
 async function fetchBalance() {
   const OPENAI_API_KEY = process.env.OPENAI_API_KEY
   const OPENAI_API_BASE_URL = process.env.OPENAI_API_BASE_URL
 
+  // 如果OPENAI_API_KEY不存在，则返回'-'
   if (!isNotEmptyString(OPENAI_API_KEY))
     return Promise.resolve('-')
 
+  // 如果OPENAI_API_BASE_URL存在，则使用它作为API基础URL，否则使用OpenAI默认的API基础URL
   const API_BASE_URL = isNotEmptyString(OPENAI_API_BASE_URL)
     ? OPENAI_API_BASE_URL
     : 'https://api.openai.com'
 
   try {
     const headers = { 'Content-Type': 'application/json', 'Authorization': `Bearer ${OPENAI_API_KEY}` }
+    // 发送请求获取账户余额，并返回结果
     const response = await axios.get(`${API_BASE_URL}/dashboard/billing/credit_grants`, { headers })
     const balance = response.data.total_available ?? 0
     return Promise.resolve(balance.toFixed(3))
   }
   catch {
+    // 如果出错了，则返回'-'
     return Promise.resolve('-')
   }
 }
 
+// 获取当前聊天机器人的配置信息
 async function chatConfig() {
   const balance = await fetchBalance()
   const reverseProxy = process.env.API_REVERSE_PROXY ?? '-'
@@ -129,13 +148,16 @@ async function chatConfig() {
   const socksProxy = (process.env.SOCKS_PROXY_HOST && process.env.SOCKS_PROXY_PORT)
     ? (`${process.env.SOCKS_PROXY_HOST}:${process.env.SOCKS_PROXY_PORT}`)
     : '-'
+  // 返回聊天机器人的配置信息
   return sendResponse<ModelConfig>({
     type: 'Success',
     data: { apiModel, reverseProxy, timeoutMs, socksProxy, httpsProxy, balance },
   })
 }
 
+// 设置代理
 function setupProxy(options: ChatGPTAPIOptions | ChatGPTUnofficialProxyAPIOptions) {
+  // 如果设置了SOCKS代理，就使用SocksProxyAgent来实例化agent对象，并将其作为fetch函数的参数传递
   if (process.env.SOCKS_PROXY_HOST && process.env.SOCKS_PROXY_PORT) {
     const agent = new SocksProxyAgent({
       hostname: process.env.SOCKS_PROXY_HOST,
@@ -146,6 +168,7 @@ function setupProxy(options: ChatGPTAPIOptions | ChatGPTUnofficialProxyAPIOption
     }
   }
   else {
+    // 如果设置了HTTP代理，就使用HttpsProxyAgent来实例化agent对象，并将其作为fetch函数的参数传递
     if (process.env.HTTPS_PROXY || process.env.ALL_PROXY) {
       const httpsProxy = process.env.HTTPS_PROXY || process.env.ALL_PROXY
       if (httpsProxy) {
@@ -158,10 +181,12 @@ function setupProxy(options: ChatGPTAPIOptions | ChatGPTUnofficialProxyAPIOption
   }
 }
 
+// 获取当前API模型
 function currentModel(): ApiModel {
   return apiModel
 }
 
+// 导出相关的类型和函数
 export type { ChatContext, ChatMessage }
 
 export { chatReplyProcess, chatConfig, currentModel }
--- service/src/index.ts
+++ service/src/index.ts
@@ -1,15 +1,48 @@
+/**
+ * 其中，import 语句导入了 express 模块以及自定义的类型和函数。在创建应用程序时，使用了 express() 函数创建了一个 Express 应用程序实例，并将其赋值给变量 app。
+
+接着，使用 express.Router() 创建了一个路由器实例，并将其赋值给变量 router。路由器是一种可以帮助处理 HTTP 请求的对象，它可以根据请求的 URL 和 HTTP 方法来选择要执行的代码。
+
+使用 app.use() 和 router.post() 函数设置了对 /chat-process、/config、/session 和 /verify 路由的处理。这些路由都是 POST 请求，因此使用了 router.post() 帮助函数。
+
+在路由上，也用到了 auth 中间件，这是一个用于身份验证的中间件函数，在处理每个请求之前都会检查是否提供了有效的身份验证令牌。
+
+express.static() 用于将应用程序与位于 "public" 目录下的静态文件相关联。
+
+express.json() 用于解析传入 JSON 数据的请求体。
+
+在设置允许跨域资源共享（CORS）的响应头时，使用了通配符 "*" 来允许任何来源的请求。
+
+在 /chat-process 路由上，读取了请求体中的提示和选项，并将其传递给 chatReplyProcess() 函数，该函数将使用 GPT 模型处理聊天，并通过响应流式传输聊天消息。
+
+/config 路由用于获取 GPT 模型的配置设置，并将其发送回客户端。
+
+/session 路由用于检查环境变量中是否设置了身份验证密钥，并向客户端发送指示是否启用了身份验证的响应。
+
+/verify 路由用于验证身份验证令牌，并向客户端发送指示验证是否成功的响应。
+
+最后，调用 app.listen() 函数，将应用程序绑定到端口 3002 上，以便能够处理来自客户端的请求。
+ */
+// 导入必要的模块和类型
 import express from 'express'
 import type { ChatContext, ChatMessage } from './chatgpt'
 import { chatConfig, chatReplyProcess, currentModel } from './chatgpt'
 import { auth } from './middleware/auth'
 import { isNotEmptyString } from './utils/is'
 
+// 创建一个 Express 应用程序
 const app = express()
+
+// 创建用于处理特定路由的路由器
 const router = express.Router()
 
+// 从 "public" 目录服务静态文件
 app.use(express.static('public'))
+
+// 解析请求体中的传入 JSON 数据
 app.use(express.json())
 
+// 为所有路由设置允许跨域资源共享（CORS）的响应头
 app.all('*', (_, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'authorization, Content-Type')
@@ -17,62 +50,83 @@ app.all('*', (_, res, next) => {
   next()
 })
 
+// 处理 POST 请求至 "/chat-process"
 router.post('/chat-process', auth, async (req, res) => {
+  // 设置响应头以指示响应为二进制文件
   res.setHeader('Content-type', 'application/octet-stream')
 
   try {
+    // 从请求体中解析出提示和选项
     const { prompt, options = {} } = req.body as { prompt: string; options?: ChatContext }
     let firstChunk = true
+    // 使用 GPT 模型处理聊天消息
     await chatReplyProcess(prompt, options, (chat: ChatMessage) => {
+      // 将聊天消息流式传输回客户端
       res.write(firstChunk ? JSON.stringify(chat) : `\n${JSON.stringify(chat)}`)
       firstChunk = false
     })
   }
   catch (error) {
+    // 将任何在聊天处理过程中遇到的错误发送回客户端
     res.write(JSON.stringify(error))
   }
   finally {
+    // 结束响应流
     res.end()
   }
 })
 
+// 处理 POST 请求至 "/config"
 router.post('/config', auth, async (req, res) => {
   try {
+    // 获取 GPT 模型的配置设置
     const response = await chatConfig()
     res.send(response)
   }
   catch (error) {
+    // 将任何在获取配置时遇到的错误发送回客户端
     res.send(error)
   }
 })
 
+// 处理 POST 请求至 "/session"
 router.post('/session', async (req, res) => {
   try {
+    // 检查环境变量中是否设置了身份验证密钥
     const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY
     const hasAuth = isNotEmptyString(AUTH_SECRET_KEY)
+
+    // 发送一个响应，指示是否启用了身份验证
     res.send({ status: 'Success', message: '', data: { auth: hasAuth, model: currentModel() } })
   }
   catch (error) {
+    // 将任何在获取会话期间遇到的错误发送回客户端
     res.send({ status: 'Fail', message: error.message, data: null })
   }
 })
 
+// 处理 POST 请求至 "/verify"
 router.post('/verify', async (req, res) => {
   try {
+    // 从请求体中检索身份验证令牌
     const { token } = req.body as { token: string }
+    // 检查令牌是否与环境变量中设置的身份验证密钥匹配
     if (!token)
       throw new Error('Secret key is empty')
 
     if (process.env.AUTH_SECRET_KEY !== token)
       throw new Error('密钥无效 | Secret key is invalid')
 
+    // 发送一个响应，指示验证成功
     res.send({ status: 'Success', message: 'Verify successfully', data: null })
   }
   catch (error) {
+    // 将任何在验证期间遇到的错误发送回客户端
     res.send({ status: 'Fail', message: error.message, data: null })
   }
 })
 
+// 将路由器挂载在应用程序上，使用基本路由 "" 和 "/api"
 app.use('', router)
 app.use('/api', router)
 
